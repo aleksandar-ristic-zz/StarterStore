8475c64faf5a0749e9fefb9faeb0d651d9861f45
aleksandar-ristic <aristic665@gmail.com>
2021-05-30 22:27:36 +0200

    Started work on backend

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const products = require('./data/products')
+
+const app = express()
+const PORT = 5000
+
+app.get('/api/products', (req, res) => {
+	res.json(products)
+})
+
+app.get('/api/product/:id', (req, res) => {
+	const product = products.find(p => p._id === req.params.id)
+	console.log(product)
+	res.json(product)
+})
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`))
