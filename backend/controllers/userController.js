93360073fe5bed081f3e183810849671d13d9f71
aleksandar-ristic <aristic665@gmail.com>
2021-06-02 00:47:48 +0200

    Added api login and register

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler'
+import generateToken from '../utils/generateToken.js'
 import User from '../models/userModel.js'
 
 // @desc   Auth user & get token
@@ -8,17 +9,70 @@ export const authUser = asyncHandler(async (req, res) => {
 	const { email, password } = req.body
 
 	const user = await User.findOne({ email })
-	console.log(await user.matchPassword(password))
+
 	if (user && (await user.matchPassword(password))) {
 		res.json({
 			_id: user._id,
 			name: user.name,
 			email: user.email,
 			isAdmin: user.isAdmin,
-			token: null
+			token: generateToken(user._id)
 		})
 	} else {
 		res.status(401)
 		throw new Error('Invalid email or password')
+	}
+})
+
+// @desc   Register a new user
+// @route  POST /users/
+// @access Public
+export const registerUser = asyncHandler(async (req, res) => {
+	const { name, email, password } = req.body
+
+	const userExists = await User.findOne({ email })
+
+	if (userExists) {
+		res.status(400)
+		throw new Error('User already exists')
+	}
+
+	const user = await User.create({
+		name,
+		email,
+		password
+	})
+
+	if (user) {
+		res.status(201).json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+			token: generateToken(user._id)
+		})
+	} else {
+		res.status(400)
+		throw new Error('Invalid user data')
+	}
+})
+
+// @desc   Auth user & get token
+// @route  GET /api/user/profile
+// @access Private
+export const getUserProfile = asyncHandler(async (req, res) => {
+	const user = await User.findById(req.user._id)
+
+	if (user) {
+		res.json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+			token: generateToken(user._id)
+		})
+	} else {
+		res.status(404)
+		throw new Error('User not found')
 	}
 })
---
+++ frontend/src/reducers/userReducers.js
@@ -0,0 +1,5 @@
+import {
+	USER_LOGIN_REQUEST,
+	USER_LOGIN_SUCCESS,
+	USER_LOGIN_FAIL
+} from '../constants/userConstants'
