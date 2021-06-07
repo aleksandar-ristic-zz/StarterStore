5005e2bf9eb865d3aaa7f11fbb3131082ee26434
aleksandar-ristic <aristic665@gmail.com>
2021-06-08 01:18:47 +0200

    Multer and image upload backend finished

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 //! You have to add .js at the end of module path in backend !
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
@@ -13,6 +14,7 @@ import connectDB from './config/database.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
 dotenv.config()
 connectDB()
@@ -20,13 +22,16 @@ connectDB()
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
 app.get('/api/config/paypal', (req, res) =>
 	res.send(process.env.PAYPAL_CLIENT_ID)
 )
 
-app.use(notFound)
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
+app.use(notFound)
 app.use(errorHandler)
 
 app.listen(
